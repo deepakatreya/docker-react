a7f4104be6f040205632945364cbe312812e42e1
deepakatreya <atreya.deepak@gmail.com>
2020-07-08 08:54:54 -0700

    Fixed breaking feature 1.0

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
+imimport React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is from feature 1.0
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          I was changed on the Feature branch
         </p>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
