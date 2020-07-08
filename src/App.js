eb546d5550a12d8e96cd27c6a0fbf44a759b7509
deepakatreya <atreya.deepak@gmail.com>
2020-07-08 08:26:32 -0700

    Changed to feature

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React. May be ? Really??
+          This is from feature 1.0
         </a>
       </header>
     </div>
